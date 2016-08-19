13dc6c588e6a1ade4edc6f1acea92eb7e4d0a6bc
Mohammad Syahid Wildan <mohammad.syahid.wildan@gmail.com>
2016-08-20 01:30:50 +0900

    show schedule

--- routes/index.js
+++ routes/index.js
@@ -10,13 +10,14 @@ var pool = mysql.createPool({
 });
 
 /* GET home page. */
+/*
 router.all('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
-router.all('/api', function (req, res, next) {
+*/
+router.all('/', function (req, res, next) {
   pool.getConnection(function (err, connection  ) {
-    var sqlForSelectList = 'SELECT * FROM userlist';
+    var sqlForSelectList = 'select username, title, Count(follower) as totalFollower from eventlist, followerlist, userlist where eventlist.eventid=followerlist.eventid AND follower = id GROUP BY followerlist.eventid';
     connection.query(sqlForSelectList, function (err, rows) {
       if (err) console.error("err : "+err);
       console.log("rows : "+JSON.stringify(rows));
