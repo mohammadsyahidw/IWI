b6e928f93c3efe848be2f42562bc54227726f035
Iffah <iffahnisrina@gmail.com>
2016-08-19 23:54:07 +0900

    database tambahan

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+var allowCORS = function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+};
+app.use(allowCORS);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var pool = mysql.createPool({
+  connectionLimit: 3,
+  host: 'localhost',
+  user: 'root',
+  database: 'webappdatabase',
+  password: '1234'
+});
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.all('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.all('/api', function (req, res, next) {
+  pool.getConnection(function (err, connection  ) {
+    var sqlForSelectList = 'SELECT * FROM userlist';
+    connection.query(sqlForSelectList, function (err, rows) {
+      if (err) console.error("err : "+err);
+      console.log("rows : "+JSON.stringify(rows));
+      res.json(rows);
+      connection.release();
+    });
+  });
+});
 module.exports = router;
