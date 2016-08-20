f7e96c35d8cc44c273f965362fb01af3a94b8797
Iffah <iffahnisrina@gmail.com>
2016-08-20 16:00:25 +0900

    asas

--- bin/client1.js
+++ bin/client1.js
@@ -1,7 +1,6 @@
 /**
  * Created by Iffah Nisrina on 8/17/2016.
  */
-var module = angular.module('myApp', []);
 /*
 module.controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
     $scope.user = {};
@@ -25,7 +24,6 @@ module.controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
 */
 var module = angular.module('myApp', []);
 module.controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
-    alert("masuk sini");
     $scope.user = {};
     $scope.user.username='';
     $http({
@@ -36,11 +34,44 @@ module.controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
 
             if (data){
                 $scope.members=data;
-                alert(data);
             } else {
             }
         })
         .error(function (data, status, headers, config) {
             console.log(status);
         });
+
+    $scope.login = function () {
+        $http({
+            method : 'POST',
+            url: 'http://localhost:3000/login',
+            data: {
+                name : $scope.namelogin,
+                password : $scope.passlogin
+            }
+        })
+            .success(function (data, status, headers, config) {
+                if (data[0].result==1){
+                    $scope.islogin = true;
+                    alert("Login Success!");
+                } else {
+                    alert("Login Failed!")
+                }
+            })
+            .error(function (data, status, headers, config) {
+                console.log(status);
+            });
+    };
 }]);
+//
+// module.directive('hideTop', function () {
+//     return {
+//         link : function (scope, element) {
+//             scope.$watch("islogin", function () {
+//                 if (scope.islogin) {
+//                     element.addClass('hide');
+//                 }
+//             });
+//         }
+//     };
+// });
