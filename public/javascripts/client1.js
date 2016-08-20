c8af9f0fc37d69169adfc8562340d5c5edbb2090
Mohammad Syahid Wildan <mohammad.syahid.wildan@gmail.com>
2016-08-21 00:11:18 +0900

    add to folder javascript

---
+++ public/javascripts/client1.js
@@ -0,0 +1,79 @@
+/**
+ * Created by Iffah Nisrina on 8/17/2016.
+ */
+/*
+ module.controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
+ $scope.user = {};
+ $scope.user.username='';
+ $http({
+ method : 'POST',
+ url: 'http://localhost:3000'
+ })
+ .success(function (data, status, headers, config) {
+
+ if (data){
+ $scope.members=data;
+ alert(data);
+ } else {
+ }
+ })
+ .error(function (data, status, headers, config) {
+ console.log(status);
+ });
+ }]);
+ */
+var module = angular.module('myApp', []);
+module.controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
+    $scope.user = {};
+    $scope.user.username='';
+    $http({
+        method : 'POST',
+        url: 'http://localhost:3000'
+    })
+        .success(function (data, status, headers, config) {
+
+            if (data){
+                $scope.members=data;
+                // console.log(data.message);
+            } else {
+            }
+        })
+        .error(function (data, status, headers, config) {
+            console.log(status);
+        });
+
+    $scope.login = function () {
+        alert("ya");
+        $http({
+            method : 'POST',
+            url: 'http://localhost:3000/login1',
+            data: {
+                name : $scope.namelogin,
+                password : $scope.passlogin
+            }
+        })
+            .success(function (data, status, headers, config) {
+                if (data == 'Success'){
+                    $scope.islogin = true;
+                    alert("Login Success!");
+                } else {
+                    alert(data);
+                }
+            })
+            .error(function (data, status, headers, config) {
+                console.log(status);
+            });
+    };
+}]);
+//
+// module.directive('hideTop', function () {
+//     return {
+//         link : function (scope, element) {
+//             scope.$watch("islogin", function () {
+//                 if (scope.islogin) {
+//                     element.addClass('hide');
+//                 }
+//             });
+//         }
+//     };
+// });
