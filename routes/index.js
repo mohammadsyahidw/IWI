7130c651027e81019d2438a227474db67688c353
Iffah <iffahnisrina@gmail.com>
2016-08-20 14:30:38 +0900

    yhha

--- routes/index.js
+++ routes/index.js
@@ -15,10 +15,10 @@ router.all('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 */
-router.all('/', function (req, res, next) {
+router.all('/api', function (req, res, next) {
   pool.getConnection(function (err, connection  ) {
-
-    var sqlForSelectList = "select username, destination, Count(follower) as totalFollower, DATE_FORMAT(date, '%d-%m-%Y') as dateValue from eventlist, followerlist, userlist where eventlist.eventid=followerlist.eventid AND follower = id GROUP BY followerlist.eventid";
+    var sqlForSelectList = "select * from eventlist";
+   // var sqlForSelectList = "select username, destination, Count(follower) as totalFollower, DATE_FORMAT(date, '%d-%m-%Y') as dateValue from eventlist, followerlist, userlist where eventlist.eventid=followerlist.eventid AND follower = id GROUP BY followerlist.eventid";
     connection.query(sqlForSelectList, function (err, rows) {
       if (err) console.error("err : "+err);
       console.log("rows : "+JSON.stringify(rows));
