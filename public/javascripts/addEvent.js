b8293a1398420e46a4fb886d886c5d3b3522fb3f
Indra Kusuma Putra <indra_kp5@yahoo.co.id>
2016-08-21 16:26:17 +0900

    menu nav

--- public/javascripts/addEvent.js
+++ public/javascripts/addEvent.js
@@ -22,20 +22,12 @@ module.controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
 
     $scope.login = function () {
         $http({
-            method : 'POST',
-            url: 'http://localhost:3000/login',
-            data: {
-                name : $scope.namelogin,
-                password : $scope.passlogin
-            }
+            method : 'GET',
+            url: 'http://localhost:3000/'
         })
             .success(function (data, status, headers, config) {
-                if (data[0].result==1){
-                    $scope.islogin = true;
-                    alert("Login Success!");
-                } else {
-                    alert("Login Failed!")
-                }
+
+                window.location = "http://localhost:3000/";
             })
             .error(function (data, status, headers, config) {
                 console.log(status);
